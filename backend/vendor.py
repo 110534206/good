04494b5b2c3b1ecbe59ed647fe9da6d5add6895a
110534206 <110534206@stu.ukn.edu.tw>
2025-11-11 15:24:35 +0800

    3

--- backend/vendor.py
+++ backend/vendor.py
@@ -2,3 +2,8 @@ from flask import Blueprint, request, jsonify, session, send_file, render_templa
 from werkzeug.utils import secure_filename
 import os
 vendor_bp = Blueprint('vendor', __name__)
+
+
+@vendor_bp.route('/vendor/resume-review')
+def vendor_resume_review():
+    return render_template('user_shared/review_resumes.html')
